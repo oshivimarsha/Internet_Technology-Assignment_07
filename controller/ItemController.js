987e67a86fc33b6305592f8956efae3fafa2e57c
oshivimarsha <oshadivimarsha@gmail.com>
2024-10-30 16:10:38 +0530

    Generated item code

--- controller/ItemController.js
+++ controller/ItemController.js
@@ -1,2 +1,18 @@
 import ItemModel from "../models/itemModel";
 import {item_arr} from "../db/database.js";
+
+
+// generated id ----------------------------------------------------------------------------------------------------
+$(document).ready(function () {
+    $("#ItemCode").val(generatedId());
+});
+
+let generatedId = function generatedId() {
+    console.log(item_arr.length + 1);
+    let code = item_arr.length + 1;
+    return "I00" + code;
+};
+
+let setItemCode = () => {
+    $("#ItemCode").val(generatedId())
+};
