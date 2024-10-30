e3bfaab375231a1253f8963605b7478bdb55d195
oshivimarsha <oshadivimarsha@gmail.com>
2024-10-30 16:15:10 +0530

    load item for table

--- controller/ItemController.js
+++ controller/ItemController.js
@@ -16,3 +16,18 @@ let generatedId = function generatedId() {
 let setItemCode = () => {
     $("#ItemCode").val(generatedId())
 };
+
+
+// load item for table ----------------------------------------------------------------------------------------------------
+const loadItemTable = () => {
+    // create table row
+    $("#itemTableBody").empty();
+    item_arr.map((item,index) => {
+        console.log(item);
+
+        let data = `<tr> <td>${item.itemCode}</td> <td>${item.itemName}</td> <td>${item.itemPrice}</td> <td>${item.itemQty}</td> </tr>`;
+        $("#itemTableBody").append(data);
+    })
+}
+
+
