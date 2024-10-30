8cbf09664cf438a22a47a9cbf317e005e32b302d
oshivimarsha <oshadivimarsha@gmail.com>
2024-10-30 11:47:47 +0530

    update Customer

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -44,3 +44,40 @@ $("#save_customer").on('click', function () {
     loadCustomerTable();
 
 });
+
+
+// update Customer------------------
+
+let selected_customer_index = null;
+
+$("#update_customer").on('click', function () {
+    let c_id = $('#CId').val();
+    let first_name = $('#FirstName').val();
+    let last_name = $('#LastName').val();
+    let mobile = $('#Mobile').val();
+    let email = $('#Email').val();
+    let address = $('#Address').val();
+
+    function generate_id() {
+        return customer_arr.length = 1;
+    }
+
+    let customer = {
+        cId : c_id,
+        firstName : first_name,
+        lastName : last_name,
+        mobile : mobile,
+        email : email,
+        address : address
+    }
+
+    // update
+    customer_arr[selected_customer_index] = customer;
+
+    // reload the table
+    loadCustomerTable();
+
+    // clear customer form
+    cleanCustomerForm();
+
+});
